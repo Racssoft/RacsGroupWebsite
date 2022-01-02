e82957a506aa6d2dd46febb54eccf7f012316db1
racssoft <93078789+RacsSoft@users.noreply.github.com>
2022-01-02 20:21:16 +0600

    update

--- main.js
+++ main.js
@@ -71,14 +71,19 @@ $(document).ready(function() {
         dots: false,
         pauseOnHover: true,
         responsive: [{
+            breakpoint: 1536,
+            settings: {
+                slidesToShow: 5
+            }
+        }, {
             breakpoint: 1280,
             settings: {
-                slidesToShow: 3
+                slidesToShow: 4
             }
         }, {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 2
+                slidesToShow: 3
             }
         }, {
             breakpoint: 768,
@@ -86,11 +91,12 @@ $(document).ready(function() {
                 slidesToShow: 2
             }
         }, {
-            breakpoint: 640,
+            breakpoint: 475,
             settings: {
                 slidesToShow: 1
             }
-        }]
+        } 
+        ]
     });
     // $("#s").click(function(){
     // 	Swal.fire({
